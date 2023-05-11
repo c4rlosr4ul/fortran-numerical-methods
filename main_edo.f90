d910599c200ec343afa00cbab205233b30ac88ad
Carlos Raul <carlos.primo@unmsm.edu.pe>
2023-05-11 16:54:20 -0500

    S.E EulerCromer 1or completed

--- main_edo.f90
+++ main_edo.f90
@@ -3,6 +3,7 @@ use solucion_edo
 
     implicit none
     real(8) :: x0, y0, dy0, x, y, y1, y2
+    real(8) :: t0, x10, x20, t, x1, x2
     integer :: n
     character(len=25) :: filename
 
@@ -10,16 +11,24 @@ use solucion_edo
     x0 = 0.0d0
     y0 = 1.0d0
     n = 100
-        x = 1.0d0
-    call m_runge_kutta(dy, x0, y0, n, x, y)
+        x = 5.0d0
 
     print *, "La solucion numerica es de la EDO de primer orden es:"
+
+    call m_euler(dy, x0, y0, n, x, y)
+
+    print *, "M. Euler: "
+    write(*,'(A4,ES15.4,1X,A7,ES20.10)') "| x=",x ,"| y(x)=",y 
+
+
+    call m_runge_kutta(dy, x0, y0, n, x, y)
+
     print *, "M. Runge Kutta: "
     write(*,'(A4,ES15.4,1X,A7,ES20.10)') "| x=",x ,"| y(x)=",y 
 
 ! Solucion de ecuaciones diferenciales de segundo orden
 
-   x0 = 0.0d0
+    x0 = 0.0d0
     y0 = 1.0d0
     dy0 = 2.0d0
     n = 10
@@ -37,7 +46,23 @@ use solucion_edo
     write(*,'(A4,1X,A1,1X,ES14.8,3X,A7,1X,A1,1X,ES14.8)') "| x", "=", x, "| y(x)", "=", y
     write(*, '(A7,1X,A1,1X,ES14.8,3X,A8,1X,A1,1X,ES14.8)') "| y'(x)", "=", y1, "| y''(x)", "=", y2
 
+    call m_verlet(d2y, x0, y0, dy0, n, x, y, y1)
+    write(*, *) "M. Verlet"
+    write(*, '(A4,1X,A1,1X,ES14.8,3X,A7,1X,A1,1X,ES14.8)') "| x", "=", x, "| y(x)", "=", y
+    write(*, '(A7,1X,A1,1X,ES14.8)') "| y'(x)", "=", y1
 
+! Sistema de edos de primer orden
+    t0 = 0.0d0
+    x10 = 100
+    x20 = 100
+    n = 20
+        t = 4.2d0
+
+    print* , "La solucion del sistema de ecuaciones difrenciales"
+    
+    call se_m_eulercromer(dx1, dx2, t0, x10, x20, n, t, x1, x2)
+    write(*, *)"M. EulerCromer"
+     write(*, '(A4,1X,A1,1X,ES14.8,3X,A7,1X,A1,1X,ES16.8,A7,1X,A1,1X,ES16.8)') "| t", "=", t, "| x1(t)", "=",x1 , "| x2(t)", "=", x2
 
 contains 
 
@@ -51,7 +76,19 @@ contains
     function dy(x, y)
         real(8), intent(in) :: x, y
         real(8) :: dy
-            dy = exp(x) - y
+            dy = 1 - x * y
+    end function
+
+    function dx1(t, x1, x2)
+        real(8), intent(in) :: t, x1, x2
+        real(8) :: dx1
+            dx1 = -0.005*sqrt(x1**2 + x2**2)*x1
+    end function
+
+    function dx2(t, x1, x2)
+        real(8), intent(in) :: t, x1, x2
+        real(8) :: dx2
+            dx2 = -9.81 - 0.005*sqrt(x1**2 + x2**2)*x2
     end function
 
 end program main_edo
--- modules/solucion_edo.f90
+++ modules/solucion_edo.f90
@@ -1,10 +1,42 @@
 module solucion_edo
     implicit none
     private
-    public :: m_euler_ad, m_runge_kutta, m_runge_kutta_4or
+    public :: m_euler, m_euler_ad, m_runge_kutta, m_runge_kutta_4or, m_verlet, se_m_eulercromer
 
 contains
-    ! Solucion de edo's de primer orden
+! Solucion de edo's de primer orden
+    subroutine m_euler(dy, x0, y0, n, x, y)
+      interface
+        real(8) function dy(x, y)
+        real(8), intent(in) :: x, y
+        end function dy
+      end interface
+        
+        real(8), intent(in) :: x0, y0, x
+        integer, intent(in) :: n
+        real(8), intent(out) :: y
+        real(8) :: norma, xt, h
+        integer :: i
+        character(len=25) :: filename
+
+        filename = "data/x_y.dat"
+
+        open(unit=90, file=filename, status="unknown", action="write")
+
+        xt = x0
+        y = y0
+        norma = abs(x - x0)/n
+        h = norma
+
+        do i = 1, n
+            y = y + h*dy(xt, y)
+            xt = xt + h
+        write(90, *) xt, y
+        end do
+
+
+    end subroutine m_euler
+
     subroutine m_runge_kutta(dy, x0, y0, n, x, y)
 
       interface
@@ -41,8 +73,7 @@ contains
 
     end subroutine m_runge_kutta
 
-
-    ! Solcuion de edo's de segundo orden
+! Solucion de edo's de segundo orden
     subroutine m_euler_ad(d2y, x0, y0, dy0, n, x, y, y1, y2)
 
     interface
@@ -76,6 +107,38 @@ contains
         end do
 
     end subroutine m_euler_ad
+
+    subroutine m_verlet(d2y, x0, y0, dy0, n, x, y, y1)
+    interface
+        real(8) function d2y(x, y, dy)
+        real(8), intent(in) :: x, y, dy
+        end function d2y
+    end interface
+        
+        real(8), intent(in) :: x0, y0, dy0, x
+        integer, intent(in) :: n
+        real(8), intent(out) :: y, y1
+        real(8) :: h, xt, dyk
+        integer :: i
+        character(len=25) :: filename
+
+        filename = "data/rk-x_y_dy.dat"
+
+        open(unit=120, file=filename, status="unknown", action="write")
+
+        h = abs(x - x0)/n
+        xt = x0
+        y = y0
+        y1 = dy0
+
+        do i = 1, n
+            dyk = d2y(xt, y, y1)
+            xt = xt + h
+            y = y + h*y1 + 0.5*h**2*d2y(xt, y,y1)
+            y1 = y1 + 0.5*h*(d2y(xt, y, y1) + dyk)
+        end do
+
+    end subroutine 
             
     subroutine m_runge_kutta_4or(d2y, x0, y0, dy0, n, x, y, y1)
     interface
@@ -123,5 +186,44 @@ contains
         end do
 
     end subroutine m_runge_kutta_4or
+
+!Solucion de sistemas de EDos de primer grado
+
+    subroutine se_m_eulercromer(dx1, dx2, t0, x10, x20, n, t, x1, x2)
+    interface
+        real(8) function dx1(t, x1, x2)
+        real(8), intent(in) :: t, x1, x2
+        end function dx1
+
+        real(8) function dx2(t, x1, x2)
+        real(8), intent(in) :: t, x1, x2
+        end function dx2
+    end interface
+        
+        real(8), intent(in) :: t0, x10, x20, t
+        integer, intent(in) :: n
+        real(8), intent(out) :: x1, x2
+        real(8) :: h, tt
+        integer :: i
+        character(len=25) :: filename
+
+        filename = "data/se-t-x1-x2.dat"
+
+        open(unit=130, file=filename, status="unknown", action="write")
+        
+        h = abs(t - t0)/n
+        tt = t0
+        x1 = x10
+        x2 = x20
+        write(130, *) tt, x1, x2
+
+        do i = 1, n
+            tt = tt + h
+            x1 = x1 + h*dx1(tt, x1, x2)
+            x2 = x2 + h*dx2(tt, x1, x2)
+        write(130, *) tt, x1, x2
+        end do
+
+    end subroutine se_m_eulercromer
 
 end module solucion_edo
